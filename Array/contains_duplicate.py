c5342182fa2cf74334d67c920933b9a4764014cb
ntnguyen1999 <nnguye97@uic.edu>
2022-11-02 02:34:24 -0500

    first 2 solve

---
+++ Array/contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        firstPointer  = 0
+        secondPointer = 1
+        sortedNum  = nums.sort()
+        if len(nums) == 1:
+            return False
+        else:
+            for i in range(0, len(nums)-1):
+                if nums[firstPointer] == nums[secondPointer]:
+                    return True
+                else:
+                    firstPointer += 1
+                    secondPointer += 1
+            return False
